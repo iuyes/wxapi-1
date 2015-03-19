46c8b488b4f63cd69e1dc67fe12391f336b96a76
mic101 <mic@wukongtv.com>
2015-03-19 22:40:29 +0800

    add json_cache

--- php/WechatVerify.php
+++ php/WechatVerify.php
@@ -9,8 +9,37 @@
  */
 class WechatVerify
 {
+    private $appId;
+    private $appSecret;
+    public function __construct($appId,$appSecret){
+        $this->appId = $appId;
+        $this->appSecret = $appSecret;
+    }
     //微信校验接口是否有效
-    public static function  checkInterfaceOK(){
+    public static function  checkInterfaceOK()
+    {
         echo $_GET['echostr'];
+    }
+    //获取Access_Token
+    public function getAccessToken(){
+        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+        $data = json_decode(file_get_contents("cache/access_token.json"));
+        if ($data->expire_time < time()) {
+            // 如果是企业号用以下URL获取access_token
+            // $url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$this->appId&corpsecret=$this->appSecret";
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
+            $res = json_decode($this->httpGet($url));
+            $access_token = $res->access_token;
+            if ($access_token) {
+                $data->expire_time = time() + 7000;
+                $data->access_token = $access_token;
+                $fp = fopen("cache/access_token.json", "w");
+                fwrite($fp, json_encode($data));
+                fclose($fp);
+            }
+        } else {
+            $access_token = $data->access_token;
+        }
+        return $access_token;
     }
 }
