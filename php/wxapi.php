74afc7614c5ca0b751e411ec0d013544b10c292d
mic101 <mic@wukongtv.com>
2015-03-19 22:26:10 +0800

    fix bug

--- php/wxapi.php
+++ php/wxapi.php
@@ -1,4 +1,3 @@
-                                                                                                                                                                                         6,30         全部
 <?php
 include_once "WechatVerify.php";
 
