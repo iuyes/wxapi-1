5afb506b3960b7acef030804ffb41717cb0468e3
mic101 <mic@wukongtv.com>
2015-03-20 15:50:48 +0800

    fix checkSignature

--- php/WechatVerify.php
+++ php/WechatVerify.php
@@ -79,4 +79,21 @@ class WechatVerify
 
         return $res;
     }
+    //验证消息的真实性
+    public function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- php/wxapi.php
+++ php/wxapi.php
@@ -9,25 +9,20 @@ $appId = "wxe4390e3ac4ce8ba4";
 $appSecret = "ca949dbe0e87c697855161c15bf56a5d";
 $pc = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
 $wechatVerify = new WechatVerify($appId,$appSecret);
-echo $wechatVerify->getJsApiTicket();
-exit;
-//get方式接收的参数
-$timeStamp = $_GET['timestamp'];
-$nonce = $_GET['nonce'];
-$msg_sign = $_GET['signature'];
-//post收到的消息密文
-$encryptMsg = $GLOBALS["HTTP_RAW_POST_DATA"];
-$xml_tree = new DOMDocument();
-$xml_tree->loadXML($encryptMsg);
-$array_e = $xml_tree->getElementsByTagName('ToUserName');
-$array_s = $xml_tree->getElementsByTagName('FromUserName');
-$tousername = $array_e->item(0)->nodeValue;
-$fromusername = $array_s->item(0)->nodeValue;
-$content = $xml_tree->getElementsByTagName('Content')->item(0)->nodeValue;
-$format = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[<a href=\"http://isogu.cn/stock/quotes/S/code/%s\">%s</a>]]></Content></xml>";
-$from_xml = sprintf($format, $fromusername,$tousername,$content,$content);
-echo $from_xml;
-
+if($wechatVerify->checkSignature()) {
+        //post收到的消息密文
+        $encryptMsg = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $xml_tree = new DOMDocument();
+        $xml_tree->loadXML($encryptMsg);
+        $array_e = $xml_tree->getElementsByTagName('ToUserName');
+        $array_s = $xml_tree->getElementsByTagName('FromUserName');
+        $tousername = $array_e->item(0)->nodeValue;
+        $fromusername = $array_s->item(0)->nodeValue;
+        $content = $xml_tree->getElementsByTagName('Content')->item(0)->nodeValue;
+        $format = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[<a href=\"http://isogu.cn/stock/quotes/S/code/%s\">%s</a>]]></Content></xml>";
+        $from_xml = sprintf($format, $fromusername, $tousername, $content, $content);
+        echo $from_xml;
+}
 exit;
 $msg = '';
 $errCode = $pc->decryptMsg($msg_sign, $timeStamp, $nonce, $encryptMsg, $msg);
