8a9312f0b39ddf90a5d34b0c6e52aa4ec3b6b558
mic101 <mic@wukongtv.com>
2015-03-20 15:02:46 +0800

    curl_httpGet

--- php/WechatVerify.php
+++ php/WechatVerify.php
@@ -42,4 +42,18 @@ class WechatVerify
         }
         return $access_token;
     }
+
+    private function httpGet($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        $res = curl_exec($curl);
+        curl_close($curl);
+
+        return $res;
+    }
 }
